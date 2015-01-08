c7c9b065b12758be8d586180be981cb769642f7c
eneim <namnh.n.aa@m.titech.ac.jp>
2015-01-08 21:35:09 +0900

    modify Main.java

--- src/im/ene/lab/rdf/bench/ksj/Main.java
+++ src/im/ene/lab/rdf/bench/ksj/Main.java
@@ -11,6 +11,13 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
+
+		/*
+		 * at titech
+		 */
+		System.setProperty("http.proxyHost", "proxy.noc.titech.ac.jp");
+		System.setProperty("http.proxyPort", "3128");
+
 		String dir = args.length > 0 ? args[0] : "shelter_data";
 
 		KsjDataManager mgr = new KsjDataManager(dir + File.separatorChar
@@ -27,26 +34,32 @@ public class Main {
 		// mgr.getBusDataset(13);
 		// mgr.getKsjFile(20);
 
-		ShelterDataset tokyoData = shelterLoader.getShelterDataset(13);
-		// System.out.println(tokyoData.toString());
+		 ShelterDataset tokyoData = shelterLoader.getShelterDataset(13);
+		 // System.out.println(tokyoData.toString());
+		//
+		 File file = new File(dir + File.separatorChar + "tokyo.txt");
+		//
+		// // if file doesnt exists, then create it
+		// if (!file.exists()) {
+		// file.createNewFile();
+		// }
+		//
+		 FileOutputStream outFile = new FileOutputStream(file);
+		//
+		// // FileWriter fw = new FileWriter(file.getAbsoluteFile(), false);
+		// // BufferedWriter bw = new BufferedWriter(fw);
+		// // bw.write(tokyoData.toString());
+		// // bw.close();
+		//
+		 tokyoData.toRDF(DataUtil.MODEL).write(outFile, "Turtle");
+		 outFile.flush();
+		 outFile.close();
 
-		File file = new File("tokyo.txt");
-
-		// if file doesnt exists, then create it
-		if (!file.exists()) {
-			file.createNewFile();
-		}
-
-		FileOutputStream outFile = new FileOutputStream(file);
-
-		// FileWriter fw = new FileWriter(file.getAbsoluteFile(), false);
-		// BufferedWriter bw = new BufferedWriter(fw);
-		// bw.write(tokyoData.toString());
-		// bw.close();
-
-		tokyoData.toRDF(DataUtil.MODEL).write(outFile, "Turtle");
-		// outFile.flush();
-		outFile.close();
+//		RandomProfileUtil profile = new RandomProfileUtil();
+//		NPeople[] profiles = profile.getDefault(); // 2 people at a time
+//
+//		if (profiles != null && profiles.length >= 1)
+//			System.out.println(DataUtil.GSON.toJson(profiles[0]));
 
 	}
 }
