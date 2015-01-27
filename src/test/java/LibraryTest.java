1788be0633cf68006bcad8bc7998befc3404b9b4
eneim <namnh.n.aa@m.titech.ac.jp>
2015-01-27 14:52:51 +0900

    reformat project for intellj

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -12,4 +12,5 @@ public class LibraryTest {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
+
 }
